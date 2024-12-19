0313003660e36786f9a28fc946fdc56aa6318e62
clementloyer <loyerclement@icloud.com>
2024-12-19 18:54:50 +0100

    Update mode.js

--- assets/js/mode.js
+++ assets/js/mode.js
@@ -11,8 +11,8 @@ function switchTheme() {
     document.documentElement.setAttribute("data-theme", "light");
     localStorage.setItem("theme", "light");
   } else {
-    document.documentElement.setAttribute("data-theme", "dark");
-    localStorage.setItem("theme", "dark");
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
   }
 }
 
